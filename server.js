85078a3092889e431599a8b01ade66c343ce767c
Tara Patterson <tarapatterson01@gmail.com>
2024-07-06 18:38:24 +0200

    finished course

--- server.js
+++ server.js
@@ -11,14 +11,24 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+const bcrypt = require('bcrypt');
 
+//Hash and Compare Passwords Asynchronously
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res);
+    });
+  });
 
+//Hash and Compare Passwords Synchronously
 //END_ASYNC
 
 //START_SYNC
-
-
-
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
 
 
